25e700a8802302e626b6cf4a61009892e65eb9f7
Eron Wright <ewright@streamnative.io>
2021-01-29 10:43:43 -0800

    Updates release tagging scripts

--- scripts/print-workspace-status.sh
+++ scripts/print-workspace-status.sh
@@ -7,9 +7,9 @@ set -o pipefail
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 VERSION_FILE=${SCRIPT_ROOT}/VERSION
 
-apiserver_builder_version=$(cat ${VERSION_FILE})
-k8s_vendor=kubernetes-1.18.4
-git_commit="$(git rev-parse HEAD)"
+apiserver_builder_version=$(git describe --tags HEAD)
+k8s_vendor=kubernetes-1.19.2
+git_commit="$(git describe --tags HEAD)"
 build_date="$(date +%Y-%m-%d-%H:%M:%S)"
 
 cat <<EOF
